165af0b0e76777b9a5c67b84281ef23a95b2ebf2
Adam Recvlohe <adam.recvlohe@pm.me>
2021-02-27 18:19:00 -0600

    Clean up code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,9 @@
 <script>
-	import successkid from 'images/successkid.jpg';
+
 </script>
 
 <style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
 
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
 </style>
 
 <svelte:head>
@@ -42,9 +12,4 @@
 
 <h1>Native and Indigenous ERGs</h1>
 
-<figure>
-	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>A collection of Native and Indigneous ERGs</p>
